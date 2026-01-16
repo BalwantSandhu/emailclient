e89da70bd7113e1f1ef1590db8d0356c98af8591
BalwantSandhu <bsingh3@unb.ca>
2026-01-16 02:23:07 -0400

    got unique username validator

--- src/app/auth/sign-up/sign-up.component.ts
+++ src/app/auth/sign-up/sign-up.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { JsonPipe } from '@angular/common';
 import { matchPassword } from '../validators/match-password.validator';
+import { uniqueUsername } from '../validators/unique-username.validator';
+import { HttpBackend, HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-sign-up',
@@ -10,13 +12,38 @@ import { matchPassword } from '../validators/match-password.validator';
   styleUrl: './sign-up.component.css'
 })
 export class SignUpComponent {
+  // Version 1 with DI for http client
+  // authForm: FormGroup;
+
+  // constructor(private http: HttpClient){
+  //   this.authForm = new FormGroup({
+  //   username: new FormControl('', [
+  //     Validators.required,
+  //     Validators.minLength(3),
+  //     Validators.maxLength(20),
+  //     Validators.pattern(/^[a-z0-9]+$/)
+  //   ], [uniqueUsername(this.http)]),
+  //   password: new FormControl('', [
+  //     Validators.required,
+  //     Validators.minLength(4),
+  //     Validators.maxLength(20)
+  //   ]),
+  //   passwordConfirmation: new FormControl('', [
+  //     Validators.required,
+  //     Validators.minLength(4),
+  //     Validators.maxLength(20)
+  //   ])
+  // }, { validators: [matchPassword]});
+  // }
+
+  // Version 2 without DI
   authForm = new FormGroup({
     username: new FormControl('', [
       Validators.required,
       Validators.minLength(3),
       Validators.maxLength(20),
       Validators.pattern(/^[a-z0-9]+$/)
-    ]),
+    ], [uniqueUsername()]),
     password: new FormControl('', [
       Validators.required,
       Validators.minLength(4),
@@ -27,6 +54,6 @@ export class SignUpComponent {
       Validators.minLength(4),
       Validators.maxLength(20)
     ])
-  }, { validators: [matchPassword]}
-);
+  }, { validators: [matchPassword]});
+
 }
---
+++ src/app/auth/validators/unique-username.validator.ts
@@ -0,0 +1,59 @@
+import { HttpClient } from "@angular/common/http";
+import { inject } from "@angular/core";
+import { AbstractControl, AsyncValidatorFn, FormControl, ValidationErrors } from "@angular/forms";
+import { catchError, map, Observable, of } from "rxjs";
+
+// Version one with DI
+// export function uniqueUsername(http: HttpClient): (control: AbstractControl) => Observable<ValidationErrors | null>{
+//     //return http.post<any>('https://api.angular-email.com/auth/username', {username: value} )
+//     return (control: AbstractControl) => {
+//         const value = control.value;
+
+//         if(!value){
+//             return of(null);
+//         }
+
+//         return http.post<any>(
+//             'https://api.angular-email.com/auth/username',
+//              {username: value}).pipe(
+//                 map(() => {
+//                     return null;
+//                 }),
+//                 catchError((err) => {
+//                     if(err.error.username){
+//                         return of({ nonUniqueUsername: true});
+//                     }
+//                     return of({ noConnection: true });
+//                 })
+//              )
+//     } 
+// }
+
+// Version 2 without DI
+export function uniqueUsername(): AsyncValidatorFn {
+  const http = inject(HttpClient);
+
+  return (control: AbstractControl) => {
+    const value = control.value;
+
+    if (!value) {
+      return of(null);
+    }
+
+    return http
+      .post<any>('https://api.angular-email.com/auth/username', {
+        username: value
+      })
+      .pipe(
+        map(() => {
+            return null;
+        }),
+        catchError((err) => {
+            if(err.error.username){
+                return of({ nonUniqueUsername: true});
+            }
+            return of({ noConnection: true });
+        })
+      );
+  };
+}
